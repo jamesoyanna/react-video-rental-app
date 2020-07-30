e193df90a3c4d61686daa6434844847afb1a5629
eshoiza <engrjayt200@gmail.com>
2020-07-30 01:40:40 +0100

    handled Onsubmit and Onchange event

--- src/component/LoginForm.jsx
+++ src/component/LoginForm.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+
+    state = {
+        account:{username: '', password: ''}
+    }
+username = React.createRef();
+
+    handleSubmit = e =>{
+        e.preventDefault();
+ const username = this.username.current.value;
+    }
+
+    handleChange = e=>{
+        const account = {...this.state};
+        account.username = e.currentTarget.value;
+        this.setState({account})
+    }
+
     render() {
         return (
             <div>
                 <h5>Login  form</h5>
-             <form  >
+             <form  onSubmit ={this.handleSubmit} >
                  <div className="form-group">
                      <label htmlFor="username">Username</label>
-                     <input id="username" type="text" className="form-control"/></div>
+                     <input
+                     onChange = {this.handleChange}
+                     value={this.state.account.username}
+                     autoFocus ref ={this.username} id="username" type="text" className="form-control"/></div>
 
                  <div className="form-group">
                      <label htmlFor="password">Password</label>
