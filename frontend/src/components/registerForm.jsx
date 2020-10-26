67107d61100644c1d1ed23fde55b00bd187e84c9
james <engrjayt200@gmail.com>
2020-10-26 15:43:41 +0100

    Handle registration error

--- frontend/src/components/registerForm.jsx
+++ frontend/src/components/registerForm.jsx
@@ -23,8 +23,18 @@ class RegisterForm extends Form {
   };
 
   doSubmit = async () => {
+    try{
+        await userService.register(this.state.data)
+    }catch(ex){
+    if(ex.response && ex.response.status=== 400){
+      const errors = {...this.state.errors}
+      errors.username = ex.response.data;
+      this.setState({errors})
+
+    }
+
+    }
     // Call the server
-    await userService.register(this.state.data)
   };
 
   render() {
