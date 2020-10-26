de5770c2d60916c6d5f4c6743d71cbb849188a2b
james <engrjayt200@gmail.com>
2020-10-26 14:25:02 +0100

    Add SaveMovie function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,39 @@
-import React from 'react';
-import Movies from './component/movies';
-import { Route, Redirect, Switch} from 'react-router-dom';
-import MoviesForm from './component/pages/MoviesForm';
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-//import http from "./component/services/httpService"
-import Rentals from "./component/pages/Rentals";
-import NotFound from "./component/pages/NotFound";
-import Customers from './component/pages/Customers';
-import NavBar from './component/pages/Navbar';
-import LoginForm from "./component/LoginForm";
-import RegisterForm from "./component/RegisterForm.jsx";
-
-import "./App.css";
+import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
-
-
-function App() {
-  return (
-    <>
-    <ToastContainer />
-    <NavBar />
-    <main className="container">
-      <Switch>
-        <Route path="/login" component={LoginForm} />
-        <Route path="/register" component={RegisterForm} />
-        <Route path="/movies/:id" component={MoviesForm} />
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound} />
-        <Redirect from="/"  exact to="/movies" />
-        <Redirect to="/not-found" />
-      </Switch>
-    </main>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-
-import App from './App';
-import logger from "./component/services/logService";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.css';
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
-logger.init();
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
-
+registerServiceWorker();
